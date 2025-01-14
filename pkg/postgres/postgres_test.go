dd9937284ac5c5f3b19f4f47c1bd77effbfbeaa5
i573718 <linus.roepert@sap.com>
2025-01-14 14:14:56 +0000

    Added tests

--- pkg/postgres/postgres_test.go
+++ pkg/postgres/postgres_test.go
@@ -6,9 +6,11 @@ package postgres
 
 import (
 	"context"
+	"encoding/json"
 	"errors"
 	"regexp"
 	"testing"
+	"time"
 
 	"github.com/pashagolub/pgxmock/v4"
 )
@@ -107,5 +109,58 @@ func TestHealthQueryRowsClosed(t *testing.T) {
 
 	if err := pool.ExpectationsWereMet(); err != nil {
 		t.Errorf("there were unfulfilled expectations: %s", err)
+	}
+}
+
+func TestParseClusterId(t *testing.T) {
+	validExample := `"shoot--project--cluster-123e4567-e89b-12d3-a456-426614174000-landscape"`
+	invalidExamples := []string{
+		`"shoot-project--cluster-123e4567-e89b-12d3-a456-426614174000-landscape"`,        // missing double dash after "shoot"
+		`"shoot--project-cluster-123e4567-e89b-12d3-a456-426614174000-landscape"`,        // missing double dash after "project"
+		`"shoot--project--cluster-123e4567-e89b-12d3-a456-42661417400-landscape"`,        // invalid UUID (one character short)
+		`"shoot--project--cluster-123e4567-e89b-12d3-a456-426614174000"`,                 // missing landscape part
+		`"sht----cluster-123esss4567-e89b-12d3-a456-426614174000-landscape-extra"`,       // broken UUID
+	}
+
+	good := EventStruct{
+		OutputFields: map[string]json.RawMessage{
+			"cluster_id": json.RawMessage(validExample),
+		},
+	}
+
+	_, err := parseClusterId(good)
+	if err != nil {
+		t.Errorf("Valid cluster ID parsing failed: %s", err)
+	}
+
+
+	for _, example := range invalidExamples {
+		bad := EventStruct{
+			OutputFields: map[string]json.RawMessage{
+				"cluster_id": json.RawMessage(example),
+			},
+		}
+
+		_, err := parseClusterId(bad)
+		if err == nil {
+			t.Errorf("Invalid cluster ID parsing succeeded: %s", example)
+		}
+	}
+}
+
+func TestBuildDeleteStatement(t *testing.T) {
+	age := time.Duration(24) * time.Hour
+	past := time.Now().UTC().Add(-age)
+
+	expected := "DELETE FROM falco_events WHERE time < $1"
+	statement, args := buildDeleteStatement(age)
+	if statement != expected {
+		t.Errorf("\nExpected statement: %s\nActual statement  : %s", expected, statement)
+	}
+
+	timeArg := time.Time(args[0].(time.Time))
+
+	if timeArg.Before(past) {
+		t.Errorf("\nExpected age ~: %s\nActual age   ~: %s", past, timeArg)
 	}
 }
